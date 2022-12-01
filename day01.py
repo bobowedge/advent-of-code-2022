44b88e58bab762285b1e70aef6455f550da5ded0
bobowedge <mmbowen@gmail.com>
2022-12-01 20:36:48 +0000

    Day 1 Solutions

--- day01.py
+++ day01.py
@@ -1,2 +1,21 @@
+def calculate_totals(data):
+    totals = []
+    total = 0
+    for d in data:
+        d = d.strip()
+        if len(d) == 0:
+            totals.append(total)
+            total = 0
+            continue
+        total += int(d)
+    return totals
 
 
+data = open("data/day01.txt").read().splitlines()
+
+totals = calculate_totals(data)
+
+print(f"Solution 1: {max(totals)}")
+
+totals.sort()
+print(f"Solution 1: {sum(totals[:3])}")
