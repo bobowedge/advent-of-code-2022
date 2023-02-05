5cb0cd3a1202f840b49d7460b24f53177ade2917
bobowedge <mmbowen@gmail.com>
2023-02-05 21:01:03 +0000

    Solution for Day 17

--- day17.py
+++ day17.py
@@ -1,8 +1,8 @@
-ROCK_TYPES = [[(0, 30), (1,  0), (2,  0), (3,  0)],  # minus shape
-          [(0,  8), (1, 28), (2,  8), (3,  0)],  # cross
-          [(0, 28), (1,  4), (2,  4), (3,  0)],  # backwards L
-          [(0, 16), (1, 16), (2, 16), (3, 16)],  # vertical line
-          [(0, 24), (1, 24), (2,  0), (3,  0)]]  # square
+ROCK_TYPES = [[(0, 30), (1, 0), (2, 0), (3, 0)],  # minus shape
+              [(0, 8), (1, 28), (2, 8), (3, 0)],  # cross
+              [(0, 28), (1, 4), (2, 4), (3, 0)],  # backwards L
+              [(0, 16), (1, 16), (2, 16), (3, 16)],  # vertical line
+              [(0, 24), (1, 24), (2, 0), (3, 0)]]  # square
 
 
 def blow_rock_step(rock, direction, filled_spots: list):
@@ -41,7 +41,7 @@ def down_rock_step(rock, filled_spots: list):
 
 
 def draw_cavern(rock, filled_spots: list, top, bottom=0):
-    for y in range(top, bottom-1, -1):
+    for y in range(top, bottom - 1, -1):
         if y < len(filled_spots):
             filled = filled_spots[y]
         else:
@@ -52,12 +52,12 @@ def draw_cavern(rock, filled_spots: list, top, bottom=0):
         else:
             print('|', end='')
             for x in range(7):
-                if filled & 2**(6-x):
+                if filled & 2 ** (6 - x):
                     print('#', end='')
                 else:
                     rock_flag = False
                     for (py, value) in rock:
-                        if py == y and value & 2**(6-x):
+                        if py == y and value & 2 ** (6 - x):
                             rock_flag = True
                     if rock_flag:
                         print("@", end='')
@@ -69,7 +69,7 @@ def draw_cavern(rock, filled_spots: list, top, bottom=0):
 
 def drop_rock(rock_type, filled_spots, data, time):
     top = len(filled_spots)
-    rock = [(y+top+3, value) for (y, value) in rock_type]
+    rock = [(y + top + 3, value) for (y, value) in rock_type]
     stop_flag = False
     while not stop_flag:
         direction = data[time % len(data)]
@@ -79,7 +79,7 @@ def drop_rock(rock_type, filled_spots, data, time):
     return rock, time
 
 
-def drop_rocks(filled_spots, time, start_rock, num_rocks):
+def drop_rocks(filled_spots, data, time, start_rock, num_rocks):
     end_rock = start_rock + num_rocks
     lentypes = len(ROCK_TYPES)
     for r in range(start_rock, end_rock):
@@ -100,7 +100,7 @@ def drop_rocks(filled_spots, time, start_rock, num_rocks):
 def solution1(data):
     filled_spots = [127]
     time = 0
-    filled_spots, _ = drop_rocks(filled_spots, time, 0, 2022)
+    filled_spots, _ = drop_rocks(filled_spots, data, time, 0, 2022)
     return len(filled_spots) - 1
 
 
@@ -128,53 +128,69 @@ def solution2(data):
             else:
                 assert False
             for i in range(7):
-                if value & 2**i:
+                if value & 2 ** i:
                     floors[i] = max(floors[i], y)
+        rock_num += 1
 
+        # Find a cycle, if there is one based on the
+        # highest blocked floor for every column (floors) +
+        # next rock type (rock_num) +
+        # current place in data (time)
         hash_rock = [f - min(floors) for f in floors]
-        hash_rock.append(rock_num % 5)
+        hash_rock.append(rock_num % lentypes)
         hash_rock.append(time % lendata)
         hash_rock = tuple(hash_rock)
         if hash_rock in hashMap.values():
             cycle_found = True
         hashMap[rock_num] = hash_rock
         heights[rock_num] = len(filled_spots) - 1
-        rock_num += 1
 
-    if rock_num == 1000000:
+    # Quit if no cycle found
+    if not cycle_found:
         print("FAIL")
         return 0
 
-    end_cycle_pn = rock_num - 1
-    end_ht = heights[end_cycle_pn]
-    end_hp = hashMap[end_cycle_pn]
+    # Record the info for the end of the cycle
+    end_cycle_rock = rock_num
+    end_cycle_height = heights[end_cycle_rock]
+    end_cycle_hash = hashMap[end_cycle_rock]
 
-    for rock_num, hp in hashMap.items():
-        if hp == end_hp and rock_num != end_cycle_pn:
-            start_cycle_pn = rock_num
-            start_ht = heights[start_cycle_pn]
+    # Calculate what rock the cycle started at
+    start_cycle_rock = None
+    start_cycle_height = None
+    for rock_num, hash_value in hashMap.items():
+        if hash_value == end_cycle_hash and rock_num != end_cycle_rock:
+            start_cycle_rock = rock_num
+            start_cycle_height = heights[start_cycle_rock]
             break
 
-    cycle_length = end_cycle_pn - start_cycle_pn
-    cycle_ht = end_ht - start_ht
+    cycle_length = end_cycle_rock - start_cycle_rock
+    cycle_height = end_cycle_height - start_cycle_height
 
+    # Rocks are dropped in three phases to get to this value
+    #   A. Beginning rocks to get to starting rock of cycle
+    #   B. Rocks that are part of one of the (many) cycles
+    #   C. Ending rocks after the last complete cycle
     FINAL_PIECE = 1000000000000
 
-    num_cycles = (FINAL_PIECE - start_cycle_pn) // cycle_length
-    remaining_rocks = FINAL_PIECE - start_cycle_pn - num_cycles * cycle_length
+    # Number of complete cycles (# of times to do B)
+    num_cycles = (FINAL_PIECE - start_cycle_rock) // cycle_length
 
-    drop_rocks(filled_spots, time, end_cycle_pn + 1, remaining_rocks)
-    new_ht = len(filled_spots) - 1
-    total_height = start_ht + num_cycles * cycle_ht + (new_ht - end_ht)
+    # Number of rocks after last complete cycle (part C)
+    ending_rocks = FINAL_PIECE - start_cycle_rock - num_cycles * cycle_length
+
+    # We're already at the end point of a cycle, so just drop the remaining rocks to get that height
+    drop_rocks(filled_spots, data, time, end_cycle_rock, ending_rocks)
+    ending_rocks_ht = len(filled_spots) - 1 - end_cycle_height
+
+    # total_height = height of part A + height of part B + height of part C
+    total_height = start_cycle_height + num_cycles * cycle_height + ending_rocks_ht
     return total_height
 
 
 data = open('data/day17.txt').read()
-data = '>>><<><>><<<>><>>><<<>>><<<><<<>><>><<>>'
+# data = '>>><<><>><<<>><>>><<<>>><<<><<<>><>><<>>'
 data = list(data)
 
 print(f"Solution 1: {solution1(data)}")
 print(f"Solution 2: {solution2(data)}")
-
-
-
